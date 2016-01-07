d965cfe046ce8960de369d904f56f323c837a6eb
Bruce Guenter <bruce@untroubled.org>
2016-01-07 08:06:58 -0600

    Flatten out the library interface, hide the reader/writer modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 mod uint32;
-pub mod hash;
-pub mod reader;
-pub mod writer;
+mod hash;
+mod reader;
+mod writer;
+
 pub use hash::hash;
+pub use reader::{CDB, Result};
+pub use writer::CDBMake;
--- tests/read.rs
+++ tests/read.rs
@@ -5,7 +5,7 @@ use std::fs;
 fn test_one() {
     //let test1[u8] = include_bytes!("tests/test1.cdb");
     let f = fs::File::open("tests/test1.cdb").unwrap();
-    let mut cdb = cdb::reader::CDB::init(f).unwrap();
+    let mut cdb = cdb::CDB::init(f).unwrap();
     let mut i = cdb.find(b"one");
     assert_eq!(i.next().unwrap().unwrap(), b"Hello");
     assert_eq!(i.next().unwrap().unwrap(), b", World!");
@@ -16,7 +16,7 @@ fn test_one() {
 fn test_two() {
     //let test1[u8] = include_bytes!("tests/test1.cdb");
     let f = fs::File::open("tests/test1.cdb").unwrap();
-    let mut cdb = cdb::reader::CDB::init(f).unwrap();
+    let mut cdb = cdb::CDB::init(f).unwrap();
     assert_eq!(cdb.find(b"two").next().unwrap().unwrap(), b"Goodbye");
     assert_eq!(cdb.find(b"this key will be split across two reads").next().unwrap().unwrap(), b"Got it.");
 }
