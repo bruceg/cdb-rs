9e6d4831d484609b79737b42672ceced8fdfdacf
Bruce Guenter <bruce@untroubled.org>
2018-07-25 21:02:54 -0600

    lib: Drop extraneous export of hash and uint32

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 extern crate filebuffer;
 
-pub mod uint32;
+mod uint32;
 mod hash;
 mod reader;
 mod writer;
 
-pub use hash::hash;
 pub use reader::{CDB, CDBIter, Result};
 pub use writer::{CDBMake, CDBWriter};
